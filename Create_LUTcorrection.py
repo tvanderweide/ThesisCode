94a73649222b6ec38a74500258b68a506af9646b
Thomas Van Der Weide <thomasvanderweide@u.boisestate.edu>
2018-10-29 08:48:42 -0600

    Updated to include Photo3/4

--- Create_LUTcorrection.py
+++ Create_LUTcorrection.py
@@ -14,43 +14,55 @@ import os
 
 #####-----------------------------------------------------------------------------------------------------------------------------------------------######
 # Read in RAW files and save as TIF
-def read_RAW(fn):
+def read_RAW(fn, cam):
     # Choose the filename you want to load
 #    filename = './Version4/Photo_2/2018_0925_234508_021.RAW'
     filename= fn
+    if cam in ['Photo_1', 'Photo_2', 'Photo_5', 'Photo_6']:
+        data = np.fromfile( filename, dtype=np.uint8 ) # read at uint8
+        data = np.unpackbits( data ) # convert to binary bits
+        datsize = data.shape[0]
+        data = data.reshape( ( int(datsize / 4), 4 ) ) # map to 4 bit rows
+        
+        
+        # Here is where rearrange the data
+        temp  = copy.deepcopy(data[0::2]) # grab every other byte from 0 to npts-1 
+        temp2 = copy.deepcopy(data[1::2]) # grab every other byte from 1 to npts-1
+        
+        # flip every other bit
+        data[0::2] = temp2
+        data[1::2] = temp
+        
+        # Now create a 16-bit number and reshape into a single array; then write to bytes  
+        udata = np.packbits(np.concatenate([data[0::3], np.array([0, 0, 0, 0] * 12000000, dtype=np.uint8).reshape(12000000,4), data[2::3], data[1::3]], axis=1).reshape(192000000, 1)).tobytes()
+        
+        # Read the bits -- 'u2' is a uint16 number; and reshape to image
+        img = np.frombuffer( udata, np.dtype('u2'), (4000*3000) ).reshape((3000, 4000))
+        
+    #    # Plot the RAW data (3000,4000); note this has not yet been debayered
+    #    plt.matshow(img, cmap=plt.cm.jet)
+    #    plt.show()
+        
+        # ### Debayer the image
+        # Potentially useful website http://answers.opencv.org/question/171179/raw-image-cvtcolor-debayer/
+        # Here are all of the different conversion methods: 
+        # http://mathalope.co.uk/2015/05/24/opencv-python-color-space-conversion-methods/
+        
+        # Debayered using the cv2 package (had to install opencv)
+        color = cv2.cvtColor(img, cv2.COLOR_BAYER_RG2RGB).astype("uint16")
+        color.shape     # __Note that the shape is now 3000 x 4000 x 3!__
+    else:
+        with open(filename, "rb") as rawimage:
+            img = np.fromfile(rawimage, np.dtype('u2'), (4000 * 3000))
     
-    data = np.fromfile( filename, dtype=np.uint8 ) # read at uint8
-    data = np.unpackbits( data ) # convert to binary bits
-    datsize = data.shape[0]
-    data = data.reshape( ( int(datsize / 4), 4 ) ) # map to 4 bit rows
+        if img.shape[0] != (4000 * 3000):
+            raise IndexError("Resolution of the image is {}. MCC only supports processing 12MP resolution. Please reset resolution to default settings.".format(img.shape[0]))
     
+        img = img.reshape((3000, 4000))
     
-    # Here is where rearrange the data
-    temp  = copy.deepcopy(data[0::2]) # grab every other byte from 0 to npts-1 
-    temp2 = copy.deepcopy(data[1::2]) # grab every other byte from 1 to npts-1
-    
-    # flip every other bit
-    data[0::2] = temp2
-    data[1::2] = temp
-    
-    # Now create a 16-bit number and reshape into a single array; then write to bytes  
-    udata = np.packbits(np.concatenate([data[0::3], np.array([0, 0, 0, 0] * 12000000, dtype=np.uint8).reshape(12000000,4), data[2::3], data[1::3]], axis=1).reshape(192000000, 1)).tobytes()
-    
-    # Read the bits -- 'u2' is a uint16 number; and reshape to image
-    img = np.frombuffer( udata, np.dtype('u2'), (4000*3000) ).reshape((3000, 4000))
-    
-#    # Plot the RAW data (3000,4000); note this has not yet been debayered
-#    plt.matshow(img, cmap=plt.cm.jet)
-#    plt.show()
-    
-    # ### Debayer the image
-    # Potentially useful website http://answers.opencv.org/question/171179/raw-image-cvtcolor-debayer/
-    # Here are all of the different conversion methods: 
-    # http://mathalope.co.uk/2015/05/24/opencv-python-color-space-conversion-methods/
-    
-    # Debayered using the cv2 package (had to install opencv)
-    color = cv2.cvtColor(img, cv2.COLOR_BAYER_RG2RGB).astype("uint16")
-    color.shape     # __Note that the shape is now 3000 x 4000 x 3!__
+        # Debayered using the cv2 package (had to install opencv)
+        color = cv2.cvtColor(img, cv2.COLOR_BAYER_RG2RGB).astype("uint16")
+        color.shape
     
 #    # Plot the channels of the debayered image
 #    plt.matshow(color[:,:,0], cmap=plt.cm.jet); plt.show()
@@ -136,7 +148,7 @@ def lut_corr(img, outfile):
 
 #####-------------------------------------------------------------------------------------------------------------------------------------------------------------#####
 # Average all the RAW images
-def avg_Raw(fold):
+def avg_Raw(fold, cam):
     # Create a list of all the RAW images
     allfiles = os.listdir(fold)
     imlist = [filename for filename in allfiles if  filename[-4:] in [".raw",".RAW"]]
@@ -150,7 +162,7 @@ def avg_Raw(fold):
     # Calculate the average for all images
     for img in imlist:
         im_path = fold + "/" + img
-        imarr = read_RAW(im_path)
+        imarr = read_RAW(im_path, cam)
         avg_arr = avg_arr + imarr/N
     
     return avg_arr
@@ -163,9 +175,10 @@ main_fold = "N:/Data02/projects-active/IGEM_Kairosys/2018 Data/Vignette/Version4
 
 
 Camera_names = ['Photo_1', 'Photo_2', 'Photo_3', 'Photo_4', 'Photo_5', 'Photo_6']
-for cam in Camera_names:
+for cam in Camera_names[2:]:
+    print("Processing: " + cam)
     fold_name = main_fold + cam
-    img_avg = avg_Raw(fold_name)
+    img_avg = avg_Raw(fold_name, cam)
     outfile = fold_name + "/lut_correction"
     lut_corr(img_avg, outfile)
 
