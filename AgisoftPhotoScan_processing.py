76169c442b7f061e3bd16164239cbd491639c017
Thomas Van Der Weide <thomasvanderweide@u.boisestate.edu>
2018-10-30 17:29:49 -0600

    Testing NDVI Transform and Cropping

--- AgisoftPhotoScan_processing.py
+++ AgisoftPhotoScan_processing.py
@@ -1,17 +1,52 @@
-# Auto batch process for Agisoft Photoscan
-# Starting code
-# https://lastexiler.wordpress.com/2017/02/22/auto-batch-processing-code-for-photoscan/
-
-# Run script in headless mode (without opening Agisoft) from command line
-# photoscan.exe -r <script.py>
+'''
+Auto batch process for Agisoft Photoscan
+ Processes all tif images into a cropped orthomosaic
+ Needs a shp and shx file in the same folder as the images to crop the orthomosaic
  
+
+Run script in headless mode (without opening Agisoft) from command line
+ photoscan.exe -r <script.py>
+
+References
+ https://lastexiler.wordpress.com/2017/02/22/auto-batch-processing-code-for-photoscan/
+ https://mapbox.s3.amazonaws.com/playground/perrygeo/rasterio-docs/cookbook.html
+'''
 import PhotoScan
-import os,re
+import os, re, time
 
 
 #####-----------------------------------------------------------------------------------------------------------------------------------#######
-def crop_ortho():
-    pass
+def crop_ortho(sf, fn):
+    try:
+        import fiona
+        import rasterio
+        from rasterio.mask import mask
+        
+        # Open the shapefile
+        with fiona.open(sf, "r") as shapefile:
+            geoms = [feature["geometry"] for feature in shapefile]
+        
+        # Read in the Tif
+        with rasterio.open(fn) as src:
+            out_image, out_transform = mask(src, geoms, crop=True)
+            out_meta = src.meta.copy()
+               
+        # Update Metadata
+        out_meta.update({"driver": "GTiff",
+                         "height": out_image.shape[1],
+                         "width": out_image.shape[2],
+                         "transform": out_transform})
+        
+        # Cropped TIF name
+        maskedFN = fn.rpartition(".")[0] + "_masked.tif"
+        # Save the Cropped TIF
+        with rasterio.open(maskedFN, "w", **out_meta) as dest:
+            dest.write(out_image)
+    
+    except:
+         print("This computer is missing something needed to crop the orthomosaic.")
+
+    return
 
 
 #####-----------------------------------------------------------------------------------------------------------------------------------#######
@@ -24,20 +59,21 @@ def getPhotoList(root_path, photoList):
                 cur_path = os.path.join(root, name)
                 #print (cur_path)
                 photoList.append(cur_path)
+    return
 
 
 #####-----------------------------------------------------------------------------------------------------------------------------------#######
-## batch process
-def photoscanProcess(root_path, save_file, save_ortho):
+## Batch Process Images in Agisoft Photoscan
+def photoscanProcess(root_path, save_file, save_ortho, NDVI_flag, CropOrtho_flag, sf):
+    # Clear the Console
+    PhotoScan.app.console.clear()
+    
     # Enable GPU Processing
     PhotoScan.app.gpu_mask = 2 ** len(PhotoScan.app.enumGPUDevices()) - 1 #setting GPU mask
     if PhotoScan.app.gpu_mask:
         PhotoScan.app.cpu_enable = False  
     else:
         PhotoScan.app.cpu_enable = True
-     
-    # Clear the Console
-    PhotoScan.app.console.clear()
     
     ## construct the document class
     doc = PhotoScan.app.document
@@ -54,29 +90,23 @@ def photoscanProcess(root_path, save_file, save_ortho):
 #    chunk = doc.chunk
     
     ## set coordinate system
-    # - PhotoScan.CoordinateSystem("EPSG::4612") --&amp;gt; JGD2000
     chunk.crs = PhotoScan.CoordinateSystem("EPSG::4326") # WGS84
     
     ################################################################################################
-    ### get photo list ###
+    ### Photo List ###
     photoList = []
     getPhotoList(root_path, photoList)
-    #print (photoList)
-    
-    ################################################################################################
-    ### add photos ###
-    # addPhotos(filenames[, progress])
-    # - filenames(list of string) – A list of file paths.
+    # Add photos to list
     chunk.addPhotos(photoList)
     
     ################################################################################################
-    ### align photos ###
+    ### Align Photos ###
     ## Perform image matching for the chunk frame.
     # matchPhotos(accuracy=HighAccuracy, preselection=NoPreselection, filter_mask=False, keypoint_limit=40000, tiepoint_limit=4000[, progress])
     # - Alignment accuracy in [HighestAccuracy, HighAccuracy, MediumAccuracy, LowAccuracy, LowestAccuracy]
     # - Reference Preselection in [ReferencePreselection, GenericPreselection, NoPreselection]
     
-    chunk.matchPhotos(accuracy=PhotoScan.HighAccuracy, preselection=PhotoScan.NoPreselection, filter_mask=False, keypoint_limit=40000, tiepoint_limit=4000)
+    chunk.matchPhotos(accuracy=PhotoScan.MediumAccuracy, preselection=PhotoScan.NoPreselection, filter_mask=False, keypoint_limit=40000, tiepoint_limit=4000)
     chunk.alignCameras()
     doc.save()
     
@@ -87,12 +117,12 @@ def photoscanProcess(root_path, save_file, save_ortho):
     # - Dense point cloud quality in [UltraQuality, HighQuality, MediumQuality, LowQuality, LowestQuality]
     # - Depth filtering mode in [AggressiveFiltering, ModerateFiltering, MildFiltering, NoFiltering]
     
-    chunk.buildDepthMaps(quality = PhotoScan.HighQuality, filter = PhotoScan.MildFiltering)
+    chunk.buildDepthMaps(quality = PhotoScan.MediumQuality, filter = PhotoScan.MildFiltering)
     chunk.buildDenseCloud(point_colors = True)
     doc.save()
     
     ################################################################################################
-    ### build mesh ###
+    ### build mesh (optional) ###
     ## Generate model for the chunk frame.
     # buildModel(surface=Arbitrary, interpolation=EnabledInterpolation, face_count=MediumFaceCount[, source ][, classes][, progress])
     # - Surface type in [Arbitrary, HeightField]
@@ -109,20 +139,17 @@ def photoscanProcess(root_path, save_file, save_ortho):
     ################################################################################################
     ### build texture (optional) ###
     ## Generate uv mapping for the model.
-    # buildUV(mapping=GenericMapping, count=1[, camera ][, progress])
-    # - UV mapping mode in [GenericMapping, OrthophotoMapping, AdaptiveOrthophotoMapping, SphericalMapping, CameraMapping]
-    #chunk.buildUV(mapping=PhotoScan.AdaptiveOrthophotoMapping)
-    ## Generate texture for the chunk.
-    # buildTexture(blending=MosaicBlending, color_correction=False, size=2048[, cameras][, progress])
-    # - Blending mode in [AverageBlending, MosaicBlending, MinBlending, MaxBlending, DisabledBlending]
-    #chunk.buildTexture(blending=PhotoScan.MosaicBlending, color_correction=True, size=30000)
-    
-    ################################################################################################
-    ## save the project before build the DEM and Ortho images
+#    #buildUV(mapping=GenericMapping, count=1[, camera ][, progress])
+#     - UV mapping mode in [GenericMapping, OrthophotoMapping, AdaptiveOrthophotoMapping, SphericalMapping, CameraMapping]
+#    chunk.buildUV(mapping=PhotoScan.AdaptiveOrthophotoMapping)
+#    # Generate texture for the chunk.
+#     buildTexture(blending=MosaicBlending, color_correction=False, size=2048[, cameras][, progress])
+#     - Blending mode in [AverageBlending, MosaicBlending, MinBlending, MaxBlending, DisabledBlending]
+#    chunk.buildTexture(blending=PhotoScan.MosaicBlending, color_correction=True, size=30000)
 #    doc.save()
     
     ################################################################################################
-    ### build DEM (before build dem, you need to save the project into psx) ###
+    ### build DEM ###
     ## Build elevation model for the chunk.
     # buildDem(source=DenseCloudData, interpolation=EnabledInterpolation[, projection ][, region ][, classes][, progress])
     # - Data source in [PointCloudData, DenseCloudData, ModelData, ElevationData]
@@ -134,7 +161,6 @@ def photoscanProcess(root_path, save_file, save_ortho):
     # buildOrthomosaic(surface=ElevationData, blending=MosaicBlending, color_correction=False[, projection ][, region ][, dx ][, dy ][, progress])
     # - Data source in [PointCloudData, DenseCloudData, ModelData, ElevationData]
     # - Blending mode in [AverageBlending, MosaicBlending, MinBlending, MaxBlending, DisabledBlending]
-#    chunk.buildOrthomosaic(surface=PhotoScan.ModelData, blending=PhotoScan.MosaicBlending, color_correction=True, projection=chunk.crs) # Uses mesh as the surface
     chunk.buildOrthomosaic(surface=PhotoScan.DataSource.ElevationData, blending=PhotoScan.MosaicBlending, projection=chunk.crs)
     doc.save()
     chunk.exportOrthomosaic(save_ortho, image_format = PhotoScan.ImageFormatTIFF)#, format = PhotoScan.RasterFormat.RasterFormatTiles)
@@ -144,14 +170,57 @@ def photoscanProcess(root_path, save_file, save_ortho):
     #chunk.dense_cloud.classifyGroundPoints()
     #chunk.buildDem(source=PhotoScan.DenseCloudData, classes=[2])
     
+    
     ################################################################################################
+    # Create NDVI of the ortho
+    # NDVI = (NIR - Red) / (NIR + Red)
+    # Export Ortho - [RasterTransformNone, RasterTransformValue, RasterTransformPalette]
+    if NDVI_flag == 1:
+        chunk.raster_transform.formula = ["(B3-B1)/(B3+B1)"]
+        chunk.raster_transform.calibrateRange()
+        chunk.raster_transform.enabled = True
+        save_NDVI = save_ortho.rpartition(".")[0] + "_NDVI.tif"
+        chunk.exportOrthomosaic(save_NDVI, image_format = PhotoScan.ImageFormatTIFF, raster_transform=PhotoScan.RasterTransformNone)
+    else:
+        pass
     
-    crop_ortho()
     
+    ################################################################################################
+    # Crop the orthomosaic to the region of interest
+    if CropOrtho_flag == 1:
+        crop_ortho(sf, save_ortho)
+        if NDVI_flag == 1:
+            crop_ortho(sf, save_NDVI)
+    else:
+        pass
+    
+    print("Finished Processing" + save_file)
+    doc.clear()
+    return
+    
+
 #####-----------------------------------------------------------------------------------------------------------------------------------#######
+# Main Function
+print("Script Started...")
+t0 = time.time()
+
 folder = "H:/Terroir/Bitner/photo/Processed_1/vigCorrected/"
-saveproj = "Bitner.psx"
-saveortho = "H:/Terroir/Bitner/photo/Processed_1/vigCorrected/BitnerOrtho.tif"
-photoscanProcess(folder, saveproj, saveortho)
+saveproj = "Bitner1.psx"
+save_ortho = "H:/Terroir/Bitner/photo/Processed_1/vigCorrected/BitnerOrtho1.tif"
+
+# NDVI from the mosaic?
+NDVI_flag = 1 # 0 - No, 1 - Yes
+
+# Crop the orthomosaic?
+CropOrtho_flag = 1 # 0 - No, 1 - Yes
+# Shape file location (Needed for cropping the ortho, just make whatever if CropOrtho = 0)
+sf = "H:/Terroir/Bitner/Bitner.shp"
+
+photoscanProcess(folder, saveproj, save_ortho, NDVI_flag, CropOrtho_flag, sf)
+
+tend = float(time.time() - t0)
+
+print("Script finished in " + "{:.2f}".format(tend) + " seconds.\n")
+    
 
 
